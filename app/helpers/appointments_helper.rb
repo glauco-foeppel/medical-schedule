1d5eeb261ae2f3a6d80cf21553dad24867a11375
Glauco Foeppel <glauco.tf@gmail.com>
2020-11-24 09:52:33 -0300

    Corrige funcionalidade de appointment para exibir horario disponivel para consultas

--- app/helpers/appointments_helper.rb
+++ app/helpers/appointments_helper.rb
@@ -1,2 +1,11 @@
 module AppointmentsHelper
+    def range_hours
+        range_hours = []
+        range_date = DateTime.now.change({hour: 9, minute: 0})
+        (1..18).each do |num|
+            range_hours << range_date
+            range_date.strftime("%H:%M").to_s == "11:30" ? range_date = range_date + 90.minutes : range_date = range_date + 30.minutes
+        end
+        range_hours
+    end
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,9 @@
 class Appointment < ApplicationRecord
   belongs_to :patient, class_name: "Patient", foreign_key: "patient_id"
   belongs_to :doctor, class_name: "Doctor", foreign_key: "doctor_id"
+
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
+  validates :patient_id, presence: true
+  validates :doctor_id, presence: true
 end
